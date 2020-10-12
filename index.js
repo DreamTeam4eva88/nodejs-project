15184556e89cbd07caaa0787ac88fd7f39ab4302
DreamTeam4eva88 <56971351+DreamTeam4eva88@users.noreply.github.com>
2020-10-12 13:34:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+var express = require("express");
+var path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+
+/**
+ *  App Configuration
+ */
+
+/**
+ *  * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+    console.log(`Listening to request on http://localhost:${port}`);
+});
